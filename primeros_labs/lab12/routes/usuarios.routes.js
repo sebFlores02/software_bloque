8566a3214b56cd1d2f5d706afe9c239ded56b1ab
FloresTM <seb.flores2002@gmail.com>
2023-03-06 23:28:27 -0600

    progreso lab 12

--- primeros_labs/lab12/routes/usuarios.routes.js
+++ primeros_labs/lab12/routes/usuarios.routes.js
@@ -5,21 +5,27 @@ const router = express.Router();
 const usuarios = [
     {
         nombre: "Benito Tagle",
+        escuela: "Texas A&M",
         pp: "https://bulma.io/images/placeholders/1280x960.png",
         user: "tagle02",
-        descripcion: "Estudio Ingenieria Mecanica en A&M"
+        descripcion: "Estudio Ingenieria Mecanica en A&M",
+        likes: 1200
     },
     {
         nombre: "Patricio Becerril",
+        escuela: "Universidad Panamericana",
         pp: "https://bulma.io/images/placeholders/1280x960.png",
         user: "becerrilp2002",
-        descripcion: "Estudio Derecho en la Universidad Panamericana"
+        descripcion: "Estudio Derecho en la Universidad Panamericana",
+        likes: 2700
     },
     {
         nombre: "Marcelo Da Paz Leal",
+        escuela: "Michigan State",
         pp: "https://bulma.io/images/placeholders/1280x960.png",
         user: "dapazleal",
-        descripcion: "Estudio Computer Science en Mich University"
+        descripcion: "Estudio Computer Science en Mich University",
+        likes: 3500
     }
 ]
 
