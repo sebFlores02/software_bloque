109f990545a466e870f81f8c7ce0a367a1eb38da
FloresTM <seb.flores2002@gmail.com>
2023-03-10 07:24:16 -0600

    lab 12

--- primeros_labs/lab12/app.js
+++ primeros_labs/lab12/app.js
@@ -1,17 +1,12 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path')
-const session = require('express-session')
 
 // llamar constructor de express
 const app = express();
 
-app.use(session({
-    secret: 'mi string secreto que debe ser un string aleatorio muy largo, no como éste',
-    resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió
-    saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
-}));
-
+// creamos carpeta estatica para accesar html, css y js
+// lo hacemos con la ruta /nombreHTML.html
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(bodyParser.urlencoded({extended: false}))
--- primeros_labs/lab12/routes/usuarios.routes.js
+++ primeros_labs/lab12/routes/usuarios.routes.js
@@ -2,16 +2,35 @@ const express = require('express');
 
 const router = express.Router();
 
-const usuarioController = require('../controllers/usuarios.controllers')
+const usuarios = [
+    {
+        nombre: "Benito Tagle",
+        escuela: "Texas A&M",
+        pp: "https://bulma.io/images/placeholders/1280x960.png",
+        user: "tagle02",
+        descripcion: "Estudio Ingenieria Mecanica en A&M",
+        likes: 1200
+    },
+    {
+        nombre: "Patricio Becerril",
+        escuela: "Universidad Panamericana",
+        pp: "https://bulma.io/images/placeholders/1280x960.png",
+        user: "becerrilp2002",
+        descripcion: "Estudio Derecho en la Universidad Panamericana",
+        likes: 2700
+    },
+    {
+        nombre: "Marcelo Da Paz Leal",
+        escuela: "Michigan State",
+        pp: "https://bulma.io/images/placeholders/1280x960.png",
+        user: "dapazleal",
+        descripcion: "Estudio Computer Science en Mich University",
+        likes: 3500
+    }
+]
 
-router.get('/nuevo', usuarioController.get_nuevo);
-
-router.post('/nuevo', usuarioController.post_nuevo);
-
-router.get('/', usuarioController.listar);
-
-// router.get('/', (request, response, next) => {
-//     response.render('home', {usuarios: usuarios});
-// });
+router.get('/', (request, response, next) => {
+    response.render('lista', {usuarios: usuarios});
+});
 
 module.exports = router;
